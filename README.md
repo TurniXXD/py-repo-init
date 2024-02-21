0d6ebf14d83b363754d1fda1c80a184a0924114e
TurniXXD <legokubik@seznam.cz>
2024-02-21 11:00:24 +0100

    cleanup

--- README.md
+++ README.md
@@ -1,24 +1,22 @@
-# py-repo-init
-Python script that initalizes a new github repository based on your init requirements and creates a new project
+# py-web-project-init
 
-## Requirements
-- `git`, `python 3`
+Python script that initializes a new github repository based on your init requirements and then makes html, css, js basic template
 
-## Besides the code you will need:
-- Generete your access token in your github account
-- Sign in with curl command:
-    `$ curl -u username:token https://api.github.com/user`
-- Give execution permission for your user on this file
-    `$ chmod +x init.py`
-- Make this file executable or add alias with path to your script into your bashrc
-- Add path into `init.py`, where your new created repos will be saved
-## Syntax 
-`$ python init.py -n REPONAME [--private, -p] [--gitignore, -g GITIGNORETYPE] [--create-react-app, -cra]`
+For this script to work you need to have installed git and python 3
 
-## Templates
-### React
-- React, Typescript, Tailwind
+## Besides the code you will need
 
-### This script is made for linux but you can always just change commands to your shitty windows Loch Ness Monster
+- generate your access token in your github account
+- sign in with curl command:
+    $ curl -u username:token <https://api.github.com/user>
+- give execution permission for your user on this file
+    $ chmod +x init.py
+- make this file executable or add alias with path to your script into your bashrc
 
-[Main inspiration](https://www.youtube.com/watch?v=b3ySWJinSh4&ab_channel=CuriousCoding)
+## Main inspiration
+
+<https://www.youtube.com/watch?v=b3ySWJinSh4&ab_channel=CuriousCoding>
+
+## Syntax
+
+$ python init.py [--name, -n REPONAME] [--private, -p] [--template, -t]
--- init.py
+++ init.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import requests
-from pprint import pprint
+
 # add your access token to secrets.py, then import here
-from secrets import GITHUB_TOKEN
+from secrets import GITHUB_TOKEN, GITHUB_USERNAME, REPO_LOCAL_PATH
 import argparse  # let you specify repo args inline
 import os
 import pathlib
@@ -14,66 +14,60 @@ parser.add_argument("--gitignore", "-ig", type=str, dest="git_ignore")
 parser.add_argument("--create-react-app", "-cra",
                     dest="is_cra", action="store_true")
 parser.add_argument("--go-server", "-go", dest="is_go", action="store_true")
-args = parser.parse_args()
+
 # args vars to be passed to payload
+args = parser.parse_args()
 repo_name = args.name
 is_private = args.is_private
 git_ignore = args.git_ignore
 is_cra = args.is_cra
 is_go = args.is_go
 
-# link to github api
 API_URL = "https://api.github.com"
-GITHUB_USERNAME = ""
-# info about repo name, desc, type
-# setting arg to true
+
 if is_private:
     payload = '{"name": "' + repo_name + '", "private": true }'
 else:
     payload = '{"name": "' + repo_name + '", "private": false }'
+
 # header api => authorization user token generated in github
 headers = {
     "Authorization": "token " + GITHUB_TOKEN,
     "Accept": "application/vnd.github.v3+json"
 }
 
-# request
 try:
     r = requests.post(API_URL+"/user/repos", data=payload, headers=headers)
-    # r.raise_for_status()
 
 
 except request.exceptions.RequestException as err:
     raise SystemExit(err)
 
 try:
-    REPO_PATH = ""  # path where your new repo will be saved
-
     def execScript(script):
         os.system("cp " + str(pathlib.Path(__file__).parent.resolve()
-                              ) + "/" + script + ".sh " + REPO_PATH + repo_name)
+                            ) + "/" + script + ".sh " + REPO_LOCAL_PATH + repo_name)
         os.system("chmod +x " + script + ".sh")
         os.system("./" + script + ".sh")
 
-    os.chdir(REPO_PATH)
+    os.chdir(REPO_LOCAL_PATH)
     os.system("mkdir " + repo_name)
-    os.chdir(REPO_PATH + repo_name)
+    os.chdir(REPO_LOCAL_PATH + repo_name)
     os.system("git init")
-    os.system("git remote add origin https://github.com/turniXXD/" +
-              repo_name + ".git")
+    os.system("git remote add origin https://github.com/" + GITHUB_USERNAME + "/" +
+            repo_name + ".git")
     os.system("echo '# " + repo_name + "' >> README.md")
     os.system(
         "echo 'run `git push -u origin master` after everything is done' >> README.md")
     os.system(
         "git add . && git commit -m 'Initial Commit' && git push -u origin master")
-    print(repo_name + ' was created in ' + REPO_PATH)
+    print(repo_name + ' was created in ' + REPO_LOCAL_PATH)
     if is_cra:
         execScript("cra")
     if is_go:
         os.system("mkdir server && cd $_")
-        os.system("printf 'module github.com/" + GITHUB_USERNAME + "/" + repo_name + "' > go.mod")
+        os.system("printf 'module github.com/" +
+                GITHUB_USERNAME + "/" + repo_name + "' > go.mod")
         execScript("go")
 except FileExistsError as err:
     raise SystemExit(err)
-
-# pprint(r.json())
